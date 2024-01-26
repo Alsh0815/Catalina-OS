64f2fe0c9d2c3b70533e04b9a610c9d0dc8021c3
Alsh <84447305+Alsh0815@users.noreply.github.com>
2024-01-26 17:35:32 +0900

    Catalina-Project 0.1.0.4

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -4,7 +4,7 @@ export PATH="$CMD_DIR":$PATH
 
 CLANG_VERSION=10
 LLD_VERSION=7
-LLVM_VERSION=14
+LLVM_VERSION=7
 
 function init() {
     apt_list=(
@@ -22,83 +22,83 @@ function init() {
         is_installed=$(which $var)
         sudo apt install $var
     done
-
-    clang_list=(
-        clang
-        clang++
-        clang-cpp
-    )
-
-    for var in ${clang_list[@]}; do
-        ln -s /usr/bin/$var-$CLANG_VERSION /usr/bin/$var >/dev/null 2>&1
-    done
-
-    lld_list=(
-        ld.lld
-        lld-link
-    )
-
-    for var in ${lld_list[@]}; do
-        ln -s /usr/bin/$var-$LLD_VERSION /usr/bin/$var >/dev/null 2>&1
-    done
-
-    llvm_list=(
-        llvm-PerfectShuffle
-        llvm-ar
-        llvm-as
-        llvm-bcanalyzer
-        llvm-cat
-        llvm-cfi-verify
-        llvm-config
-        llvm-cov
-        llvm-c-test
-        llvm-cvtres
-        llvm-cxxdump
-        llvm-cxxfilt
-        llvm-diff
-        llvm-dis
-        llvm-dlltool
-        llvm-dwarfdump
-        llvm-dwp
-        llvm-exegesis
-        llvm-extract
-        llvm-lib
-        llvm-link
-        llvm-lto
-        llvm-lto2
-        llvm-mc
-        llvm-mca
-        llvm-modextract
-        llvm-mt
-        llvm-nm
-        llvm-objcopy
-        llvm-objdump
-        llvm-opt-report
-        llvm-pdbutil
-        llvm-ranlib
-        llvm-rc
-        llvm-readelf
-        llvm-readobj
-        llvm-rtdyld
-        llvm-size
-        llvm-split
-        llvm-stress
-        llvm-strings
-        llvm-strip
-        llvm-symbolizer
-        llvm-tblgen
-        llvm-undname
-        llvm-xray
-    )
-
-    for var in ${llvm_list[@]}; do
-        ln -s /usr/bin/$var-$LLVM_VERSION /usr/bin/$var >/dev/null 2>&1
-    done
 }
 
 if [[ $1 = "--init" ]]; then
     init
 fi
+
+clang_list=(
+    clang
+    clang++
+    clang-cpp
+)
+
+for var in ${clang_list[@]}; do
+    ln -s /usr/bin/$var-$CLANG_VERSION /usr/bin/$var >/dev/null 2>&1
+done
+
+lld_list=(
+    ld.lld
+    lld-link
+)
+
+for var in ${lld_list[@]}; do
+    ln -s /usr/bin/$var-$LLD_VERSION /usr/bin/$var >/dev/null 2>&1
+done
+
+llvm_list=(
+    llvm-PerfectShuffle
+    llvm-ar
+    llvm-as
+    llvm-bcanalyzer
+    llvm-cat
+    llvm-cfi-verify
+    llvm-config
+    llvm-cov
+    llvm-c-test
+    llvm-cvtres
+    llvm-cxxdump
+    llvm-cxxfilt
+    llvm-diff
+    llvm-dis
+    llvm-dlltool
+    llvm-dwarfdump
+    llvm-dwp
+    llvm-exegesis
+    llvm-extract
+    llvm-lib
+    llvm-link
+    llvm-lto
+    llvm-lto2
+    llvm-mc
+    llvm-mca
+    llvm-modextract
+    llvm-mt
+    llvm-nm
+    llvm-objcopy
+    llvm-objdump
+    llvm-opt-report
+    llvm-pdbutil
+    llvm-ranlib
+    llvm-rc
+    llvm-readelf
+    llvm-readobj
+    llvm-rtdyld
+    llvm-size
+    llvm-split
+    llvm-stress
+    llvm-strings
+    llvm-strip
+    llvm-symbolizer
+    llvm-tblgen
+    llvm-undname
+    llvm-xray
+)
+
+for var in ${llvm_list[@]}; do
+    ln -s /usr/bin/$var-$LLVM_VERSION /usr/bin/$var >/dev/null 2>&1
+done
 
 cd edk2
 source edksetup.sh
