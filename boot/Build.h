486c73971488b2d7827c86d05c680552354b0331
Alsh <84447305+Alsh0815@users.noreply.github.com>
2024-02-01 05:48:33 +0900

    Catalina-Project 0.1.1.0

--- boot/Build.h
+++ boot/Build.h
@@ -5,10 +5,10 @@
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
 #define PATCH_VERSION 0
-#define REVIS_VERSION 1
+#define REVIS_VERSION 2
 
 #define BUILD_YEAR 2024
-#define BUILD_MONTH 1
-#define BUILD_DATE 31
+#define BUILD_MONTH 2
+#define BUILD_DATE 1
 
 #endif
--- boot/Main.c
+++ boot/Main.c
@@ -1,3 +1,4 @@
+#include <Guid/FileInfo.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
@@ -51,11 +52,53 @@ EFI_STATUS UefiMain(
     EFI_FILE_PROTOCOL *root_dir;
     OpenRootDir(ImageHandle, &root_dir);
 
-    EFI_FILE_PROTOCOL *memmap_file;
-    root_dir->Open(root_dir, &memmap_file, L"\\memmap", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    EFI_FILE_PROTOCOL *kernel_file;
+    root_dir->Open(
+        root_dir, &kernel_file, L"\\kernel.elf",
+        EFI_FILE_MODE_READ, 0);
 
-    SaveMemoryMap(&memmap, memmap_file);
-    memmap_file->Close(memmap_file);
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_info_buffer[file_info_size];
+    kernel_file->GetInfo(
+        kernel_file, &gEfiFileInfoGuid,
+        &file_info_size, file_info_buffer);
+
+    EFI_FILE_INFO *file_info = (EFI_FILE_INFO *)file_info_buffer;
+    UINTN kernel_file_size = file_info->FileSize;
+
+    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+    gBS->AllocatePages(
+        AllocateAddress, EfiLoaderData,
+        (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+    kernel_file->Read(kernel_file, &kernel_file_size, (VOID *)kernel_base_addr);
+    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
+    EFI_STATUS status;
+
+    status = gBS->ExitBootServices(ImageHandle, memmap.map_key);
+    if (EFI_ERROR(status))
+    {
+        status = GetMemoryMap(&memmap);
+        if (EFI_ERROR(status))
+        {
+            Print(L"failed to get memory map: %r\n", status);
+            while (1)
+                ;
+        }
+        status = gBS->ExitBootServices(ImageHandle, memmap.map_key);
+        if (EFI_ERROR(status))
+        {
+            Print(L"Could not exit boot service: %r\n", status);
+            while (1)
+                ;
+        }
+    }
+
+    UINT64 entry_addr = *(UINT64 *)(kernel_base_addr + 24);
+
+    typedef void EntryPointType(void);
+    EntryPointType *entry_point = (EntryPointType *)entry_addr;
+    entry_point();
 
     return EFI_SUCCESS;
 }
